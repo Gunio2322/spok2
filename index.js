f3e0425d3143d6df87fbd4de03bdf443333ed84b
Mariusz G <gunio2322@gmail.com>
2023-01-10 17:13:50 +0100

    czysc

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 require("dotenv").config()
 // const { request } = require('express')
 const express = require('express')
+const handlebars = require('express-handlebars')
+
 const PORT = process.env.PORT || 3300
 const app = express()
 // Pliki statyczne
 app.use(express.static(__dirname + '/public'))
 
+// silnik szablonow handlebars
+app.engine('handlebars', expressHandlebars({ extname: '.hbs' }))
+app.set('view engine', '.hbs')
+
+// przykladowy tekst
 app.get('*', (req, res) => {
 res.type('text/plain')
 res.send("hello word")})
-
+// app.get((req, res) => {
+// res.render('*', {
+//   message: 'Witaj, szanowny programisto!',
+//   style: req.query.style,
+//   userid: req.cookies.userid,
+//   username: req.session.username
+// })
+// })
 
 // Niestandardowa strona 404
 app.use((req, res) => {
@@ -24,6 +38,7 @@ app.use((req, res) => {
     res.status(500)
     res.send('500 - Błąd serwera')
   })
+  // nasluchiwanie servera
   app.listen(PORT, () => console.log(
     `Express został uruchomiony pod adresem http://localhost:${PORT}; ` +
     `naciśnij Ctrl-C, aby zakończyć.`))
